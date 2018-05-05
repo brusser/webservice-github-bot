dfd33eca63b33759c4f6fa28017ff6f7171dbf20
Michael Hewitt <brusser@gmail.com>
2018-05-05 16:03:53 -0700

    bug in data name issue not issues

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -11,10 +11,10 @@ router = routing.Router()
 
 @router.register("issues", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
-    """ 
+    """
     Whenever an issue is opened, greet the author and say thanks.
     """
-    url = event.data["issues"]["comments_url"]
+    url = event.data["issue"]["comments_url"]
     author = event.data["issue"]["user"]["login"]
 
     message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
